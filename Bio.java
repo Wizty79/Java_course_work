4fcf969ebdd1ec32b2e431fcc00ceb151389f5cc
Wizty79 <wistikwistisen@gmail.com>
2022-06-15 15:12:49 +0000

    edit

---
+++ Bio.java
@@ -0,0 +1,14 @@
+public class Bio {
+    public static void main(String[] args) {
+        String name = "my name is Wisti";
+        int age = 42;
+        String country = " I'm from Denmark & live in the UK";
+        String sport = " I absolutely don't play sports";
+        String hours = "unsure";
+        String game = "PC games";
+        String subject = "hu?";
+        char grade = 'A';
+        System.out.println(name + country + sport);
+
+    }
+}
