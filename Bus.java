6d00ffcac8e14267306e367be2d6a3f551d840cd
Wizty79 <wistikwistisen@gmail.com>
2022-06-15 14:52:17 +0000

    edit

--- Bus.java
+++ Bus.java
@@ -1,14 +1,13 @@
 public class Bus {
     public static void main(String[] args) {
         String sentence = "Number of passengers: ";
-        System.out.println(sentence);
         int passengers = 0;
         passengers += 9; //passsengers = passengers + 9 is the same result
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
         passengers -= 5;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
         passengers -= 4;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
     }
     
 }
---
+++ Form.java
@@ -0,0 +1,19 @@
+public class Form {
+    public static void main(String[] args) {
+        char gender = 'F';
+        String name = "Jenna";
+
+        System.out.println("Gender: " + gender);
+        System.out.println("Name: " + name);
+    }
+}
+
+/* char = charecter
+   the single quotes '' are for char values, "" double quotes are for string values 
+   in the first version of the code we wrote $char name = 'Jenna'
+   you can use that or String, however char consumes less memory 
+   char is a primitive data type, where String is an object
+   so rule is:
+   use char to store single character
+   use String to store larger text
+*/
