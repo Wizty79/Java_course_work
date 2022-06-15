4a0fab284a0196ef84ad327c54be21d892f5fd44
Wizty79 <wistikwistisen@gmail.com>
2022-06-15 13:20:53 +0000

    edit

---
+++ Bus.java
@@ -0,0 +1,16 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passengers = 0;
+        passengers += 9; //passsengers = passengers + 9 is the same result
+        System.out.println(passengers);
+        passengers -= 5;
+        System.out.println(passengers);
+        passengers -= 4;
+        System.out.println(passengers);
+    }
+    
+}
+
+// Tip: You can run the comman clear to clear the terminal 
+
+// =+ operator updates by adding the value on the right
