008b1bbd199e995b18f0b495866f80eaabc30f92
Wizty79 <wistikwistisen@gmail.com>
2022-06-15 13:25:28 +0000

    java edit duda

--- Bus.java
+++ Bus.java
@@ -1,5 +1,7 @@
 public class Bus {
     public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
+        System.out.println(sentence);
         int passengers = 0;
         passengers += 9; //passsengers = passengers + 9 is the same result
         System.out.println(passengers);
