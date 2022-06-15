fc43a4cddf26b713bcf264fe82afd38b7eda8b41
Wizty79 <wistikwistisen@gmail.com>
2022-06-15 08:42:38 +0000

    first duda

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
+
+/*when you've written new code, always compile your code with the command javac
+then run the compiled code with java
+if you make a change and run the code without compiling, it will only run the old code
+always compile 
+*/
