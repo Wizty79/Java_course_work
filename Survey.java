4fbfc0eabaa793c91f33b55c817ad48fbbe4a257
Wizty79 <wistikwistisen@gmail.com>
2022-06-16 14:46:46 +0000

    jave madness

---
+++ Survey.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Survey {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome. Thank you for taking the survey");
+        System.out.println("What is your name?");
+        String name  = scan.nextLine();
+    }
+}
