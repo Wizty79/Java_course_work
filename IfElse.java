5a5a783c8f7ce2d80d2b5b6bedd24026a9a2d44f
Wizty79 <wistikwistisen@gmail.com>
2022-06-17 15:30:29 +0000

    edit

--- IfElse.java
+++ IfElse.java
@@ -2,7 +2,7 @@ public class IfElse {
     public static void main(String[] args) {
         int grade = 49;
 
-        if (grade > 50) {
+        if (grade >= 50) {
             System.out.println("Great job, you pass!");
         } else {
             System.out.println("Study harder next time!");
