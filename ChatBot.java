f95d8a38d2ec1ff337953b0d1da3c45181b572e5
Wizty79 <wistikwistisen@gmail.com>
2022-06-17 11:38:26 +0000

    edit

--- ChatBot.java
+++ ChatBot.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class ChatBot {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello! What's your name?");
+        System.out.println("\nHello! What's your name?");
         String name = scan.nextLine();
 
-        System.out.println("Hi " + name + "Where are you from?")
+        System.out.println("\nHi " + name + " Where are you from?");
         String place = scan.nextLine();
 
-        System.out.println(" I hear it's beautiful in " + place + "I'm from a place called Viborg" + "\n How old are you?")
+        System.out.println("\n I hear it's beautiful in " + place + " I'm from a place called Viborg" + "\n How old are you?");
         double age = scan.nextDouble();
 
-        System.out.println("So your're " + age + "I'm 400 years old!");
+        System.out.println("\nSo your're " + age + " I'm 400 years old!" + "\nThis means I'm " + (400/age) + " times older then you");
+        
+        System.out.println("\nEnough about me. What's your favorite language?");
+        String languageChoice = scan.nextLine();
+
+        System.out.println(languageChoice + " that's great! Nice chatting with you" + name + "I have to log off now. See ya!");
+        
     }
 }
