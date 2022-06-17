d8d80097819735a8dcbbe235b50b2f82b94d4911
Wizty79 <wistikwistisen@gmail.com>
2022-06-17 12:26:29 +0000

    java edit

--- ChatBot.java
+++ ChatBot.java
@@ -3,21 +3,27 @@ import java.util.Scanner;
 public class ChatBot {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("\nHello! What's your name?");
         String name = scan.nextLine();
 
         System.out.println("\nHi " + name + " Where are you from?");
         String place = scan.nextLine();
 
-        System.out.println("\n I hear it's beautiful in " + place + " I'm from a place called Viborg" + "\n How old are you?");
-        double age = scan.nextDouble();
-
-        System.out.println("\nSo your're " + age + " I'm 400 years old!" + "\nThis means I'm " + (400/age) + " times older then you");
+        System.out.println("\n I hear it's beautiful in " + place + ". I'm from a place called Viborg"); 
         
-        System.out.println("\nEnough about me. What's your favorite language?");
+        System.out.println("\n How old are you?");
+        int age = scan.nextInt();
+
+        System.out.println("\nSo your're " + age + " I'm 400 years old!"); 
+        
+        System.out.println("\nThis means I'm " + (400/age) + " times older then you");
+        
+        System.out.println("Enough about me. What's your favorite language?");
+        scan.nextLine();
         String languageChoice = scan.nextLine();
 
-        System.out.println(languageChoice + " that's great! Nice chatting with you" + name + "I have to log off now. See ya!");
+        System.out.println(languageChoice + " that's great! Nice chatting with you " + name + ". I have to log off now. See ya!");
         
     }
 }
