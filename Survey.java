d77d8a33444ffbd1705bce58f98c8dc83701c515
Wizty79 <wistikwistisen@gmail.com>
2022-06-17 10:34:33 +0000

    edit

--- Survey.java
+++ Survey.java
@@ -5,20 +5,29 @@ public class Survey {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome. Thank you for taking the survey");
 
+        int counter = 0;
+
         System.out.println("What is your name?");
         String name  = scan.nextLine();
+        counter++;
 
         System.out.println("How much money do you spend on coffee?");
         double coffeePrice = scan.nextDouble();
+        counter++;
 
         System.out.println("How much money do you spend on fast food?");
         double foodPrice = scan.nextDouble();
+        counter++;
 
         System.out.println("How many times a week do you buy coffee?");
         int coffeeAMount = scan.nextInt();
-
+        counter++;
+        
         System.out.println("How many times a week do you buy fast food?");
         int foodAmount = scan.nextInt();
+        counter++;
+
+        System.out.println("Thank you " + name + " for answering all " + counter + "questions");
 
         scan.close();
     }
