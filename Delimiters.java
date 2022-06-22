abd17ffb815813b6697485965c4ce80b6fd91ecd
Wizty79 <wistikwistisen@gmail.com>
2022-06-22 11:44:57 +0000

    duda

--- Delimiters.java
+++ Delimiters.java
@@ -7,17 +7,23 @@ public class Delimiters {
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
 
-        System.out.println("Enter two very big integers (on the saje line)");
+        System.out.println("Enter two very big integers (on the same line)");
         long bigNum1 = scan.nextLong();
         long bigNum2 = scan.nextLong();  
 
-        System.out.println("Enter two decimals (on the saje line)");
-        double decimal1 = scan.nextLong();
-        double decimal2 = scan.nextLong();
+        System.out.println("Enter two decimals (on the same line)");
+        double decimal1 = scan.nextDouble();
+        double decimal2 = scan.nextDouble();
 
-        System.out.println("Enter two text values (on the saje line)");
+        System.out.println("Enter two text values (on the same line)");
         String text1 = scan.next();
         String text2 = scan.next();
         
+        scan.close();
+
+        System.out.println("\tIntegers: " + num1 + " " + num2);
+        System.out.println("\tBig integers: " + bigNum1 + " " + bigNum2);
+        System.out.println("\tDecimals: " + decimal1 + " " + decimal2);
+        System.out.println("\tWords: " + text1 + " " + text2);
     }
 }
