62f6e21f5f1098fbc229f6ebeeba9fe092a422e7
Wizty79 <wistikwistisen@gmail.com>
2022-06-22 08:19:01 +0000

    edit

--- LogicalOperatores.java
+++ LogicalOperatores.java
@@ -5,9 +5,18 @@ public class LogicalOperatores {
         String language = "Java";
 
         if (chemistryGrade > 75 || englishGrade > 75 || language.equals("Java")) {           //shows the use of the or || operator
-            System.out.println(" COngratulations, you got the scholarship!");
+            System.out.println(" Congratulations, you got the scholarship!");                       //if just one condition is tgrue, it outputs 
         } else {
             System.out.println("We're sorry, you didn't get the scholarship.");
+        }
+
+        int credits = 56;
+        double GPA = 3.2;
+
+        if (credits >= 40 && GPA >= 2.0) {
+            System.out.println("You earned you diploma!");
+        } else {
+            System.out.println("Sorry, you need at leats 40 credits and a minimum GPA of 2.0");    //however with the and && operator, both have to be true to trigger the output 
         }
         
     }
