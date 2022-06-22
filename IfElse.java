f09c7a185bf36175a7e45e1acd4184d168d7ac30
Wizty79 <wistikwistisen@gmail.com>
2022-06-22 07:58:21 +0000

    java duda

--- IfElse.java
+++ IfElse.java
@@ -1,12 +1,19 @@
 public class IfElse {
     public static void main(String[] args) {
-        int grade = 49;
+        int grade = 65;
 
-        if (grade >= 50) {
-            System.out.println("Great job, you pass!");
+        if (grade >= 80) {
+            System.out.println("You got an A");
+        } else if (grade >= 70) {
+            System.out.println("You got a B");
+        } else if (grade >= 60 ) {
+            System.out.println("You got a C");
+        } else if (grade >= 50) {
+            System.out.println("You got a D");
         } else {
             System.out.println("Study harder next time!");
-        } 
-        
+        }
+
     }
 }
+
