1e6f51b49ff647dc7ea238608aba6b71fe4d2962
Wizty79 <wistikwistisen@gmail.com>
2022-06-22 12:19:22 +0000

    edit

--- Dealership.java
+++ Dealership.java
@@ -8,7 +8,25 @@ public class Dealership {
         System.out.println(" .Select option 'b' to sell a car");
         String option = scan.nextLine(); 
         switch (option) {
-            case "a": System.out.println("You chose option " + option); break;
+            case "a": 
+                System.out.println("What is your budget");
+                int budget = scan.nextInt();
+                if (budget >= 10000) {
+                    System.out.println("Great! A NIssan Altima is avalible");
+                    System.out.println("Do you have insurance? Write 'Yes' or 'No'");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a license? Write 'Yes' or 'No'");
+                    String license = scan.nextLine();
+                    System.out.println("\nWhat is your credit score");
+                    int creditScore = scan.nextInt();
+                    if (insurance.equals("Yes") && license.equals("Yes") && creditScore > 660);
+                        System.out.println("Sold! Pleasure doing business with you");
+                } else{
+                    System.out.println("We're sorry. You are not eligible");
+                }
+            break;
+
             case "b": System.out.println("You chose option " + option); break;
             default: System.out.println("invalid option");
         }
