196d6f8c1beb948b56eed411f57dda8a85754f5d
Wizty79 <wistikwistisen@gmail.com>
2022-06-22 08:11:16 +0000

    duda

--- LogicalOperatores.java
+++ LogicalOperatores.java
@@ -2,8 +2,9 @@ public class LogicalOperatores {
     public static void main(String[] args) {
         int chemistryGrade = 78;
         int englishGrade = 65;
+        String language = "Java";
 
-        if (chemistryGrade > 75 || englishGrade > 75) {           //shows the use of the or || operator
+        if (chemistryGrade > 75 || englishGrade > 75 || language.equals("Java")) {           //shows the use of the or || operator
             System.out.println(" COngratulations, you got the scholarship!");
         } else {
             System.out.println("We're sorry, you didn't get the scholarship.");
