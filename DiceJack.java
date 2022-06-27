89206e61632768680931175280f4e607a66e2db9
Wizty79 <wistikwistisen@gmail.com>
2022-06-27 10:57:31 +0000

    edit

--- DiceJack.java
+++ DiceJack.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class DiceJack {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in); 
         int roll1 = rollDice();
         int roll2 = rollDice();
         int roll3 = rollDice();
 
-        System.out.println(roll1);
-        System.out.println(roll2);
-        System.out.println(roll3);
+        System.out.println("Enter three numbers between 1 and 6");
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+
+        if (num1 < 1 || num2 < 1 || num3 < 1) {
+            System.out.println("Numbers cannot be less than 1. SHutting the game down");
+            System.exit(0);
+        }
+
+        if  (num1 > 6 || num2 > 6 || num3 > 6 ) {
+            System.out.println("NUmbers cannot be greater than 6. SHutting the gamne down");
+            System.exit(0);
+        }
+
+        scan.close();
         
     }
     public static int rollDice() {
