e9736e3b7a0c76e4ed18e1758c4689fae94873b3
Wizty79 <wistikwistisen@gmail.com>
2022-06-27 09:06:10 +0000

    java duda

---
+++ FunctionsBuildInFunctions.java
@@ -0,0 +1,9 @@
+public class FunctionsBuildInFunctions {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+        System.out.println("The sin of 1.2 is: " + sine);
+        
+    }
+}
---
+++ Scope.java
@@ -0,0 +1,9 @@
+public class Scope {
+    public static void main(String[] args) {
+        someFunction();
+    }
+    public static void someFunction() {
+        int apples = 5;
+        System.out.println(apples);
+    }
+}
