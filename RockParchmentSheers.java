cc4954def85ea9d9ec27e3951afb3a6ad36027b9
Wizty79 <wistikwistisen@gmail.com>
2022-06-27 13:40:01 +0000

    edit

---
+++ RockParchmentSheers.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class RockParchmentSheers {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Let's play Rock PArchment SHeers.");
+        System.out.println("When I say 'shoot', CHoose: rock, parchment, or sheers");
+        System.out.println("Are you ready? Write 'yes' if you are.");
+
+        String ready = scan.next();
+
+        if (ready.equals("yes")) {
+            System.out.println("\nGreat!");
+            System.out.println("rock - parchment - sheers, shoot! ");
+            String choice = scan.nextLine();
+        } else {
+            System.out.println("Darn, we'll play some other time.");
+        }
+
+        scan.close();
+
+       public static String computerChoice() {
+       double randomNumber = Math.random() * 3; 
+       int integer = (int)randomNumber; 
+
+       switch (integer) {
+                case 0: return "rock";
+                case 1: return "parchment";
+                case 2: return "Sheers";
+                default: return "";
+        }
+        }    
+
+    }
+}
