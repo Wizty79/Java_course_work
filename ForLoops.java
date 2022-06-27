e278566f394ccf0b5e2f616dfd7cede7d40cc181
Wizty79 <wistikwistisen@gmail.com>
2022-06-27 14:54:15 +0000

    edit

--- ForLoops.java
+++ ForLoops.java
@@ -1,5 +1,19 @@
 public class ForLoops {
     public static void main(String[] args) {
-        
+        for ( int i = 1; i <= 3; i++) {
+            System.out.println(i);
+        }
     }
 }
+
+
+
+/* use a for loop when you know in advance 
+how many times a pice a code needs to run
+
+user a while loop when it's not clear
+how many times a pice of code needs to run
+*/
+
+
+
