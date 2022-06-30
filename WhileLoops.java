ed29acb3937ee297f2da82f00943adeb20ee4116
Wizty79 <wistikwistisen@gmail.com>
2022-06-30 10:41:00 +0000

    java loops lesson

---
+++ WhileLoops.java
@@ -0,0 +1,28 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        for (int number = 25; number <= 30; number++ ) {
+            System.out.println(number);
+        }
+        
+    }
+}
+
+
+/* 
+public class WhileLoops {
+    public static void main(String[] args) {
+        int number = 25;
+        while (number <= 30){
+            System.out.println(number);
+            number++;
+        }
+        
+    }
+}
+*/
+
+/*same result fort both the for and while loop
+but as we know the specific number of times this loops should run
+we need to use the for loop, which obviously also is cleqaner and easier to use in this case.
+ */
+
